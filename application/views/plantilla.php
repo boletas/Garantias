739c5d73878b86d812ad8185ce963c2412e2d622
Admin <Admin@Admin-PC>
2015-03-04 12:53:47 -0300

    archivo donde se caragan los css y js

--- application/views/plantilla.php
+++ application/views/plantilla.php
@@ -7,7 +7,17 @@
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <title>...:::Boletas Garantia:::...</title>
         <link rel="stylesheet" href="<?=base_url();?>assets/css/bootstrap.css"/>
+        <link rel="stylesheet" href="<?=base_url();?>assets/css/metisMenu.css"/>
+        <link rel="stylesheet" href="<?=base_url();?>assets/css/sb-admin-2.css"/>
+        <link rel="stylesheet" href="<?=base_url();?>assets/css/morris.css"/>
+        <link rel="stylesheet" href="<?=base_url();?>assets/css/font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="<?=base_url();?>assets/css/bootstrap-theme.min.css"/>
+        
+        <script src="<?=base_url();?>assets/js/sb-admin-2.js"></script>
+        <script src="<?=base_url();?>assets/js/metisMenu.js"></script>
+        <script src="<?=base_url();?>assets/js/raphael-min.js"></script>
+        <script src="<?=base_url();?>assets/js/morris.min.js"></script>
+        <script src="<?=base_url();?>assets/js/morris-data.js"></script>
         <script src="<?=base_url();?>assets/js/jquery-1.11.2.min.js"></script>
         <script src="<?=base_url();?>assets/js/bootstrap.min.js"></script>
     </head>
