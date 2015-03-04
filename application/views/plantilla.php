cec62baa7f23cf6adce0f0ee67339a49f5f34a3f
italo araya <iaraya@procesac.com>
2015-03-04 10:29:33 -0300

    Empezando con menu lateral y menu vertical

--- application/views/plantilla.php
+++ application/views/plantilla.php
@@ -8,7 +8,6 @@
         <title>...:::Boletas Garantia:::...</title>
         <link rel="stylesheet" href="<?=base_url();?>assets/css/bootstrap.css"/>
         <link rel="stylesheet" href="<?=base_url();?>assets/css/bootstrap-theme.min.css"/>
-        <link rel="stylesheet" href="<?=base_url();?>assets/css/simple-sidebar.css"/>
         <script src="<?=base_url();?>assets/js/jquery-1.11.2.min.js"></script>
         <script src="<?=base_url();?>assets/js/bootstrap.min.js"></script>
     </head>
