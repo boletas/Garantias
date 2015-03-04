b0197977e519cb133c15fa8de8a499e365caf55e
italo araya <iaraya@procesac.com>
2015-03-04 09:36:39 -0300

    Empezando con menu lateral y menu vertical

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,13 +1,3 @@
-</div>
-</div>
-<!-- /#page-content-wrapper -->
-</div>
-<!-- /#wrapper -->
-<script>
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#wrapper").toggleClass("toggled");
-});
-</script>
+
 </body>
 </html>
