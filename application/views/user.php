3c4fa4314d8c900414e0bed9d5dd87ff449e1d50
Admin <Admin@Admin-PC>
2015-03-04 12:54:03 -0300

    para probar que funciona la weaita

--- application/views/user.php
+++ application/views/user.php
@@ -1,2 +1,17 @@
-<?php echo $this->session->userdata('usuario')?>
-<a href="<?php echo base_url();?>index.php/logout_controller/Cerrar_sesion">cerrar sesion</a>
+
+
+        <div id="page-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">Boletas Garantias</h1>
+                        <?php echo $this->session->userdata('usuario')?>
+                        <a href="<?php echo base_url();?>index.php/logout_controller/Cerrar_sesion">cerrar sesion</a>
+
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /#page-wrapper -->
+</div>
+    <!-- /#wrapper -->
