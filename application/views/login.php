d25a777c7aef5c09e18f94783378d7e1347df9e7
BoletasGarantias <boletas.garantia@gmail.com>
2015-04-05 20:37:55 -0400

    indicadores

--- application/views/login.php
+++ application/views/login.php
@@ -59,6 +59,28 @@
             </div>
         </div>
     </div>
+    <?php
+$xmlSource = "http://indicadoresdeldia.cl/webservice/indicadores.xml";
+$xml = simplexml_load_file($xmlSource);
+echo $xml->santoral->ayer;
+echo $xml->santoral->hoy;
+echo $xml->santoral->maniana;
+echo $xml->moneda->dolar;
+echo $xml->moneda->euro;
+echo $xml->moneda->dolar_clp; //Dolar interbancario
+echo $xml->indicador->ivp;
+echo $xml->indicador->uf;
+echo $xml->indicador->ipc;
+echo $xml->indicador->utm;
+echo $xml->indicador->imacec;
+echo $xml->bolsa->ipsa;
+echo $xml->bolsa->igpa;
+echo $xml->bolsa->banca;
+echo $xml->bolsa->commodities;
+echo $xml->bolsa->retail;
+echo $xml->bolsa->consumo;
+echo $xml->bolsa->utilities;
+?>
 </div> <!-- ** FIN CONTAINER ** -->
 <script src="<?=base_url();?>assets/js/custom.modernizr.js"></script>
 <script src="<?=base_url();?>assets/js/custom.js"></script>
