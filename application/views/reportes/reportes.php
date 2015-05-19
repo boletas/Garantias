079ab5d356c6919033ba93e0330ec26d6dff6574
BoletasGarantias <boletas.garantia@gmail.com>
2015-05-18 21:52:11 -0400

    trabajando en buscador de boletas

--- application/views/reportes/reportes.php
+++ application/views/reportes/reportes.php
@@ -2,4 +2,14 @@
     <div class="col-lg-12">
         <h1 class="page-header">Reportes</h1>
     </div>
+    <div class="col-lg-10 col-lg-offset-1">
+        <div class="panel panel-default">
+            <div class="panel-heading"> Seleccione Tipo de Busqueda</div>
+            <div class="panel-body">
+                <div class="form-group">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
