58a6900d4ba2997ea1722951e10a11e801a50cdf
elias <elias@NB-CHINO>
2015-09-09 09:19:19 -0300

    Ultimos detalles

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -1,13 +1,13 @@
 <div class="row">
     <br/><br/><br/>
-    <div class="col-lg-8 col-lg-offset-2">
+    <div class="col-lg-10 col-lg-offset-1">
         <blockquote>
             <p class="lead text-uppercase text-primary"><img src="<?php echo base_url()?>assets/img/favicon.jpg"/>&nbsp;&nbsp;Sistema de Control de Boletas en Garantia</p>
         </blockquote>
     </div>
     <div class="col-lg-12"><br/><br/><br/><br/></div>
     <?php if($this->session->userdata('fin_mes') == 1 && $ingreso == 1){ ?>
-        <div class="col-lg-6 col-lg-offset-2">
+        <div class="col-lg-10 col-lg-offset-1">
             <div class="alert alert-info" role="alert" align="center">
                 El ingreso de los indicadores mensuales se encuentran disponible. <button type="button" onclick="Indicadores()" class="btn btn-outline btn-primary btn-sm">Aquí</button>
             </div>
