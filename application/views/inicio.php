5554ac16ed62774d1703318ac630246647d2ac80
elias <elias@NB-CHINO>
2015-09-08 23:31:17 -0300

    Ultimos detalles

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -1,88 +1,14 @@
 <div class="row">
     <br/><br/><br/>
-    <?php if($indicadores){ ?>
-        <div class="col-lg-3 col-md-6">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-9 text-right">
-                            <div class="huge">$<?php echo $indicadores['uf']; ?></div>
-                        </div>
-                    </div>
-                </div>
-                <a href="http://www.bcentral.cl/index.asp" target="_blank">
-                    <div class="panel-footer">
-                        <span class="pull-left">UF</span>
-                        <span class="pull-right"><i class="fa  fa-info-circle"></i></span>
-                        <div class="clearfix"></div>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-9 text-right">
-                            <div class="huge">$<?php echo $indicadores['utm']; ?></div>
-                        </div>
-                    </div>
-                </div>
-                <a href="http://www.bcentral.cl/index.asp" target="_blank">
-                    <div class="panel-footer">
-                        <span class="pull-left">UTM</span>
-                        <span class="pull-right"><i class="fa fa-info-circle"></i></span>
-                        <div class="clearfix"></div>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-9 text-right">
-                            <div class="huge">$<?php echo $indicadores['dolar']; ?></div>
-                        </div>
-                    </div>
-                </div>
-                <a href="http://www.bcentral.cl/index.asp" target="_blank">
-                    <div class="panel-footer">
-                        <span class="pull-left">Dólar Observado</span>
-                        <span class="pull-right"><i class="fa fa-info-circle"></i></span>
-                        <div class="clearfix"></div>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-9 text-right">
-                            <div class="huge">$<?php echo $indicadores['euro']; ?></div>
-                        </div>
-                    </div>
-                </div>
-                <a href="http://www.bcentral.cl/index.asp" target="_blank">
-                    <div class="panel-footer">
-                        <span class="pull-left">Euro</span>
-                        <span class="pull-right"><i class="fa  fa-info-circle"></i></span>
-                        <div class="clearfix"></div>
-                    </div>
-                </a>
-            </div>
-        </div>
-    <?php }else{ ?>
-<!--        <div class="col-lg-8 col-lg-offset-2">
-            <div class="alert alert-warning" role="alert">
-                No se lograron obtener los indicadores, al parecer existen problemas con su conexión a Internet.
-            </div>
-        </div>-->
-    <?php } ?>
+    <div class="col-lg-8 col-lg-offset-2">
+        <blockquote>
+            <p class="lead text-uppercase text-primary"><img src="<?php echo base_url()?>assets/img/favicon.jpg"/>&nbsp;&nbsp;Sistema de Control de Boletas en Garantia</p>
+        </blockquote>
+    </div>
+    <div class="col-lg-12"><br/><br/><br/><br/></div>
     <?php if($this->session->userdata('fin_mes') == 1 && $ingreso == 1){ ?>
-        <div class="col-lg-8 col-lg-offset-2">
-            <div class="alert alert-info" role="alert">
+        <div class="col-lg-6 col-lg-offset-2">
+            <div class="alert alert-info" role="alert" align="center">
                 El ingreso de los indicadores mensuales se encuentran disponible. <button type="button" onclick="Indicadores()" class="btn btn-outline btn-primary btn-sm">Aquí</button>
             </div>
         </div>
